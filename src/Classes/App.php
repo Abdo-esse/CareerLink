fc8f38f281100d0c4b48758919bece9ad01e9e9e
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-02 23:26:03 +0100

    j'ai termin navbar

---
+++ src/Classes/App.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Classes;
+
+class App {
+    public function greet() {
+        return "Hello from the App class!";
+    }
+}
---
+++ src/Views/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php'; 
+include __DIR__ . '/pages/navbar.php';
+
+// use App\Classes\App; 
+
+// $app = new App();
+// echo $app->greet();
