ec49b9f041808310de757d884db9625e4142e8e4
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-05 14:55:01 +0100

    j'ai selecte les values de inpute

--- src/Controllers/auth/signupRecruteur.php
+++ src/Controllers/auth/signupRecruteur.php
@@ -1,2 +1,22 @@
-<?php 
-echo 'gfgfggf';
+<?php
+
+
+
+require __DIR__ . '/../../../vendor/autoload.php'; 
+use App\Classes\Role; 
+use App\Classes\Candidat;
+use App\model\Validation;
+
+
+if(isset($_POST["SignUP"]))
+{
+   echo 'abdo';
+    $name=$_POST["name"];
+    $nomEntreprise=$_POST["nomEntreprise"];
+    $emailProfessionnel=$_POST["emailProfessionnel"];
+    $email=$_POST["email"];
+    $password=$_POST["password"];
+    $passwordRepate=$_POST["passwordRepate"];
+    $role= new Role(2,"Recruteur");
+   
+}
