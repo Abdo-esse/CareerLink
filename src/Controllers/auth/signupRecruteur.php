8509db516ac390a9c0864e4e3859c5ba680724d8
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-05 16:16:54 +0100

    j'ai termin  validation de  connexion  de recruteur

--- src/Controllers/auth/signupRecruteur.php
+++ src/Controllers/auth/signupRecruteur.php
@@ -18,11 +18,51 @@ if(isset($_POST["SignUP"]))
     $password=$_POST["password"];
     $passwordRepate=$_POST["passwordRepate"];
     $role= new Role(2,"Recruteur");
-    
+    if(!empty( $name)||!empty($email)||!empty( $password)||!empty($passwordRepate)||!empty($nomEntreprise)||!empty($emailProfessionnel))
+    {
+        if(Validation::validationUsername($name)==false)
+        {
+            // echo "invalid name !";
+            header("location: ../../Views/auth/candidatPage.php??error=invaldname");
+            exit();
+        }
+        if(Validation::validationNomEntreprise($nomEntreprise)==false)
+        {
+            // echo "invalid name !";
+            header("location: ../../Views/auth/candidatPage.php??error=invaldnameEntreprise");
+            exit();
+        }
+        if(Validation::validationEmail($email)==false)
+        {
+            // echo "invalid email !";
+            header("location: ../../Views/auth/candidatPage.php??error=invalidemail");
+            exit();
+        }
+        if(Validation::validationEmail($emailProfessionnel)==false)
+        {
+            // echo "invalid email !";
+            header("location: ../../Views/auth/candidatPage.php??error=invalidemail");
+            exit();
+        }
+        if(Validation::passwordMatch($password,$passwordRepate)==false)
+        {
+            // echo "Passworde don't match !";
+            header("location: ../../Views/auth/candidatPage.php??error=PasswordDontMatch");
+            exit();
+        }
+        if(!Validation::checkUser($name,$email))
+    {
+        header("location: ../../Views/auth/candidatPage.php?had_Smiya_oula_email_existe");
+        exit();
+    }
+    $hashedPwd= password_hash($password,PASSWORD_DEFAULT);
     $recruteur= new Recruteur($name, $email, $password,$role,$nomEntreprise ,$emailProfessionnel);
     $recruteur->inscription();
     header("Location: ../../Views/index.php?iyh_3lamolana_rak_wlit_recruteur");
     exit(); 
+    }
+
+   
     
     
 }
