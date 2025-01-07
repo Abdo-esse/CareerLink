046f3ef16c9e7d28760bf150566e0fc7c93b8838
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-07 01:19:53 +0100

    j'ai termine la creation de class tag

---
+++ src/Views/admin/gestionTags/addTag.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add Tag</title>
+      <!-- tailwind -->
+    <!-- carousel -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"
+      rel="stylesheet"
+    />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <!-- sweetalert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              clifford: "#da373d",
+            },
+          },
+        },
+      };
+    </script>
+    <style type="text/tailwindcss">
+      @layer utilities {
+        .content-auto {
+          content-visibility: auto;
+        }
+      }
+    </style>
+</head>
+<body>
+    
+<div class="min-h-screen bg-purple-400 flex justify-center items-center">
+
+  <form action="../../../Controllers/admin/addCategorie.php" method="post" class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+    <div>
+      <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Add Tag</h1>
+      <p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">Ajoutez une nouvelle tag ici.</p>
+    </div>
+    <div class="space-y-4">
+      <input type="text" name="name" placeholder="Ajouter une categorie" class="block text-sm py-3 px-4 rounded-lg w-full border outline-purple-500" />
+    </div>
+    <div class="text-center mt-6">
+      <button type="submit" name="submit" class="w-full py-2 text-xl text-white bg-purple-400 rounded-lg hover:bg-purple-500 transition-all">Add Tag</button>
+    </div>
+  </form>
+
+</div>
+</body>
+</html>
--- src/Views/admin/gestionTags/tags.php
+++ src/Views/admin/gestionTags/tags.php
@@ -125,7 +125,7 @@
     </div>
     <div class="button-add-club p-2" >
       <button class="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" >
-        <a href="addCategorie.php"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <a href="addTag.php"><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
 </svg>
 </a>
