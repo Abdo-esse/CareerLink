9d5ab22a87051209c44d6cc3bb678b90257cde09
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-07 11:16:14 +0100

    j'ai termine le style de formulaire Sign up candidate

--- src/Views/auth/candidatPage.php
+++ src/Views/auth/candidatPage.php
@@ -4,23 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up CAndidat</title>
+      <!-- tailwind -->
+    <!-- carousel -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"
+      rel="stylesheet"
+    />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <!-- sweetalert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              clifford: "#da373d",
+            },
+          },
+        },
+      };
+    </script>
+    <style type="text/tailwindcss">
+      @layer utilities {
+        .content-auto {
+          content-visibility: auto;
+        }
+      }
+    </style>
 </head>
 <body>
-    <h1>Candidat</h1>
-    <form action="../../Controllers/auth/signupCandidat.php" method="post">
-    <label for="fullName">Full name</label>
-    <input type="text" name="name" required placeholder="Full name" id="fullName"><br>
+    
+<div class="min-h-screen bg-gray-900 flex justify-center items-center">
 
-    <label for="email">Email</label>
-    <input type="email" name="email" required placeholder="votre e-mail" id="email"><br>
-
-    <label for="password">Password</label>
-    <input type="password" name="password" required id="password"><br>
-
-    <label for="password2">Repeat Password</label>
-    <input type="password" name="passwordRepate" required id="password2"><br>
-
-    <input type="submit" name="SignUP" value="Sign Up">
-</form>
+  <form action="../../Controllers/auth/signupCandidat.php" method="post" class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+    <div>
+      <h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Log in</h1>
+      <p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">Sign up now to enjoy all services without ads, completely free!</p>
+    </div>
+    <div class="space-y-4">
+      <input type="text" name="name" required placeholder="Full name" class="block text-sm py-3 px-4 rounded-lg w-full border outline-purple-500" />
+      <input type="email" name="email" required placeholder="votre e-mail" class="block text-sm py-3 px-4 rounded-lg w-full border outline-purple-500" />
+      <input type="password" name="password" required  class="block text-sm py-3 px-4 rounded-lg w-full border outline-purple-500" />
+      <input type="password" name="passwordRepate" required class="block text-sm py-3 px-4 rounded-lg w-full border outline-purple-500" />
+    </div>
+    <div class="text-center mt-6">
+      <button type="submit" name="SignUP"  class="w-full py-2 text-xl text-white bg-purple-400 rounded-lg hover:bg-purple-500 transition-all">Sign up</button>
+      <p>You have an account? <a href=""> Log in →</a></p>
+    </div>
+  </form>
+  
+</div>
 </body>
 </html>
--- src/Views/auth/login.php
+++ src/Views/auth/login.php
@@ -1,28 +1,3 @@
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-</head>
-<body>
-    <form action="../../Controllers/auth/login.php" method="post">
-        <label for="email">E-mail</label> <br>
-        <input type="email"  name="email" id="email" required placeholder="exemple@gmail.com"><br>
-        <label for="password">password</label><br>
-        <input type="password"  name="password"id="password" required ><br>
-       
-        <input type="submit" name="login">
-    </form>
-</body>
-</html> -->
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
