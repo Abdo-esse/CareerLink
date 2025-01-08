1b81d253fae31ce9cd0a0d3621d2caeca149008e
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-01-08 10:04:08 +0100

    J'ai termin la rucupration de donner du forme de update

--- src/model/Crud.php
+++ src/model/Crud.php
@@ -44,6 +44,20 @@ class Crud
 
         return $stmt->fetchAll(PDO::FETCH_OBJ);
       }
+      static function readOffre($id){
+        $conn = Connexion::connexion(); 
+        $sql="SELECT offres_emploi.id, offres_emploi.poste,offres_emploi.date_delete,offres_emploi.salaire,offres_emploi.qualifications_requises,offres_emploi.lieu_travail,offres_emploi.date_create,categories.name as categorie ,categories.id as idCategorie,GROUP_CONCAT(tags.name) as tags
+              FROM offres_emploi
+              JOIN offre_emploi_tags on offre_emploi_tags.id_offre_emploi=offres_emploi.id
+              JOIN tags ON tags.id=offre_emploi_tags.id_tag
+              JOIN categories on categories.id=offres_emploi.id_categorie
+              Where offres_emploi.id=?
+              GROUP BY offres_emploi.id;";
+              $stmt=$conn->prepare($sql);
+              $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_OBJ);
+      }
       static function readAction($table,$id){
         $conn = Connexion::connexion();
         $sql="SELECT * FROM $table WHERE id= ?";
